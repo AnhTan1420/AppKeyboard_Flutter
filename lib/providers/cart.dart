36786f7cd2bc2493d823de2f3f0d643aa45d9520
Tonny <nguyenxuantung473@gmail.com>
2021-12-31 16:50:24 +0700

    [tungnx] finish provider cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -15,7 +15,7 @@ class CartItem {
   });
 }
 class Cart with ChangeNotifier {
-  final Map<String, CartItem> _items = {};
+  late Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
@@ -25,5 +25,43 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  void addItem(
+      String productId,
+      double price,
+      String title,
+      ) {
+    if (_items.containsKey(productId)) {
+      // change quantity...
+      _items.update(
+        productId,
+            (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+            () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+    notifyListeners();
+  }
 
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
 }
