ccaaacbded56585d5c01627c1ed439faa0238614
Tonny <nguyenxuantung473@gmail.com>
2021-12-31 16:35:46 +0700

    [tungnx] create cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart' show Cart;
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Đơn Hàng Của Tôi'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryTextTheme.headline6!.color,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    child: const Text('ORDER NOW'),
+                    onPressed: () {
+
+                    },
+                    textColor: Theme.of(context).primaryColor,
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+
+        ],
+      ),
+    );
+  }
+}
